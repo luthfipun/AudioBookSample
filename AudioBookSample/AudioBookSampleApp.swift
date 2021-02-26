6156b6174c5ac82d82e01e9cdb3e4f522a171e5b
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-02-26 19:09:29 +0700

    Initial Commit

---
+++ AudioBookSample/AudioBookSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookSampleApp.swift
+//  AudioBookSample
+//
+//  Created by Luthfi Abdul Azis on 26/02/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
